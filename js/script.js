11124ec392f9e12e3767bb111a9209239076cd88
bascopeespinoza0 <bascopeespinozagabriel@fpllefia.com>
2024-11-01 21:21:23 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 console.log("running");
 
-const apiUrl =  "https://api.adviceslip.com/advice";
 const txt = document.querySelector('p#text');
 const adviceId = document.querySelector('span#adviceId');
 const diceBtn = document.querySelector('div.btnDice');
 
 diceBtn.addEventListener('click', function() {    
+    const random = Math.floor(Math.random() * 224) + 1;
+    const apiUrl =  `https://api.adviceslip.com/advice/${random}`;
+
     //GET request from api
     fetch(apiUrl)
-    .then(Response =>{
-        if (!Response.ok) {
-            throw "Error";
-        }
-        return Response.json();
-    })
-    .then(Data =>{
-        console.log(Data);
-        adviceId.textContent = Data.slip.id;
-        txt.textContent = `"${Data.slip.advice}"`;
-    })
-    .catch(err =>{
-        console.error(err);
-    });
+        .then(Response =>{
+            if (!Response.ok) {
+                throw "Error";
+            }
+            return Response.json();
+        })
+        .then(Data =>{
+            adviceId.textContent = Data.slip.id;
+            txt.textContent = `"${Data.slip.advice}"`;
+        })
+        .catch(err =>{
+            console.error(err);
+        });
 
 });
